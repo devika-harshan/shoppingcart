de8253e5ff0af54bfbd66421173d90f89efe478e
Devika Harshan <85967317+devika-harshan@users.noreply.github.com>
2021-06-16 03:04:16 +0530

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,24 @@
+let counter = 1
+const unitPrice = document.getElementById("disc-pri").innerHTML;
+document.getElementById("inc").addEventListener("click", function () {
+    counter++;
+    document.getElementById("qnty").value = counter;
+    const price = document.getElementById("disc-pri");
+    const finprice =document.getElementById("fp");
+    price.innerHTML = (unitPrice * counter).toFixed(2);
+    finprice.innerHTML=(unitPrice * counter).toFixed(2);
+
+})
+
+document.getElementById("dec").addEventListener("click", function () {
+    counter--;
+    document.getElementById("qnty").value = counter;
+    const price = document.getElementById("disc-pri");
+    const finprice =document.getElementById("fp");
+    price.innerHTML = (unitPrice * counter).toFixed(2);
+    finprice.innerHTML=(unitPrice * counter).toFixed(2);
+
+})
+// // document.getElementById("fp").innerText = unitPrice;
+// let discount = document.getElementById("fd").innerText;
+// let delchg = document.getElementById("fdel").innerText;
